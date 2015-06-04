4c2f99e492e9b3df9ed9ff661c5a7b06a3337334
c1887 <xxlassexx@web.de>
2015-06-04 17:42:18 +0200

    Revert "row sums instead of col sums"
    This reverts commit dd5f9412251772df8ba8d41180a96da6cf3ecfcb.

--- modules/MSM.py
+++ modules/MSM.py
@@ -17,8 +17,8 @@ def markovProp(T):
     d1, d2 = T.shape
     if not (d1 == d2):
         return False, "not a square matrix"
-    if not np.array_equal(np.sum(T,2), np.ones(len(T))):
-        return False, "rows don't sum up to one"
+    if not np.array_equal(np.sum(T,1), np.ones(len(T))):
+        return False, "columns don't sum up to one"
     return True, "all good"
 
 def commClasses(g):
