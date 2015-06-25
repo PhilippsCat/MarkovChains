4464d9980b7cab072dbf96f69cca80079015a5e7
nwulkow <niklas.wulkow@ewetel.net>
2015-06-25 15:37:09 +0200

    reg space clustering

---
+++ regspace.py
@@ -0,0 +1,10 @@
+def regspace(X , dmin):
+
+    T = X.shape[1]
+    Y = list()
+    Y.append(X[:,0])
+    
+    for t in range(2,T):
+        if ( np.min ( np.linalg.norm(X[:,t] - Y)) < dmin):
+            Y.append(X[:,t])
+    return Y
