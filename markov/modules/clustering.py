076d2aff5511f6a75fbafa64d25d166d8204b7b0
Johannes Sauer <jtsauer@gmail.com>
2015-06-25 12:39:07 +0000

    changed k-means function to make compatible with the mcmm module

--- markov/modules/clustering.py
+++ markov/modules/clustering.py
@@ -3,38 +3,38 @@ import numpy as np
 def kmeans(data, k):
     """
     Performs a k-means clustering on a given data set, which should have the
-    dimension d x n, where d is the dimension of each data point and n is the
+    dimension n x d, where d is the dimension of each data point and n is the
     total number of data points. k is the number of clusters.
-    Usage: kmeans(data, k)
     Return: Array that maps each data point to a cluster, array which
     contains the centers of the clusters.
     """
-    n = np.shape(data)[1] #suppose data steps are in y direction  
-    means = data[:,np.random.choice(n,k)]
+    n = np.shape(data)[0] 
+    means = data[np.random.choice(n,k)]
 
-    def closest_means(current_means):
-        associated_mean = np.ones(n)
+    def closest_means(means):
+        assoc_means = np.ones(n)
         for i in range(0,n):
-            dists = np.linalg.norm(np.vstack(data[:,i])-current_means, axis=0)
-            associated_mean[i] = np.argmin(dists)
-        return associated_mean
+            dists = np.linalg.norm(np.repeat(data[i],k,axis=0)-means,axis=1)
+            assoc_means[i] = np.argmin(dists)
+        return assoc_means
     
-    def calculate_centroids(means):
+    def calculate_centroids(point_to_means):
+        new_means = means
         for i in range(0,k):
                 # if there is no point associated to the current mean,
-                #don't change anything
+                # don't change anything
                 if np.sum(point_to_means==i)==0:        
                     continue
-                means[:,i]=np.sum(data[:, point_to_means==i],1)/np.sum(point_to_means==i)
-        return means
+                new_means[i] = np.sum(data[point_to_means==i],0)/np.sum(point_to_means==i)
+        return new_means
 
-    changed = True
     ptm_old = np.zeros(n)
-    while changed: 
+    while True: 
         point_to_means = closest_means(means)            
+        #TODO: check if arrays ar almost equal
         if np.array_equal(ptm_old, point_to_means):
-            changed = False
+            break
         ptm_old = point_to_means
-        means = calculate_centroids(means)
+        means = calculate_centroids(point_to_means)
     
     return point_to_means, means
