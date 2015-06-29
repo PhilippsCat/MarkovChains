dd995d053f1f34fff42e410ea8a026d57e23ca5e
nwulkow <niklas.wulkow@ewetel.net>
2015-06-29 13:08:14 +0200

    New version of regspace

--- regspace.py
+++ regspace.py
@@ -1,10 +1,42 @@
+import numpy as np
+
 def regspace(X , dmin):
 
-    T = X.shape[1]
+
+    
+    # Reshape the data 
+    if (len(X.shape) == 2):
+        len_X = max( X.shape[0], X.shape[1])
+    else:
+        len_X = len(X)
+    X = X.reshape(-1,len_X)
+    T = len_X
     Y = list()
     Y.append(X[:,0])
     
-    for t in range(2,T):
-        if ( np.min ( np.linalg.norm(X[:,t] - Y)) < dmin):
+    # Do the regular space algorithm
+    for t in range(1,T):
+        if ( np.min ( np.absolute(X[:,t]*np.ones(len(Y)) - Y)) > dmin):
             Y.append(X[:,t])
-    return np.asarray(Y)
+     
+    # The format of Y is changed from list to np.array
+    Y = np.asarray(Y)
+
+    
+    closest_centers = np.zeros(T)
+    # values are mapped to their closest centers
+    for i in range(0,T):
+        closest_center_index = np.argmin( np.absolute(X[:,i]*np.ones(len(Y)) - Y.transpose()))
+        closest_centers[i] = Y[closest_center_index]
+    
+    print closest_centers
+    # 'uniques' is the list of states that occur in 'closest_centers'
+    uniques = list(set(closest_centers))
+    # we map the values in 'closest_centers' to integer values
+    int_states = np.zeros(T)
+    for j  in range(0,T):
+        int_states[j] = uniques.index(closest_centers[j])
+
+    
+    
+    return int_states, Y
