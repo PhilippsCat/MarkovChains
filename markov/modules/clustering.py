6078fb4d27ef14c0f93cbf340c0ff2956add9241
nwulkow <niklas.wulkow@ewetel.net>
2015-06-29 13:09:47 +0200

    Update clustering.py

--- markov/modules/clustering.py
+++ markov/modules/clustering.py
@@ -39,14 +39,4 @@ def kmeans(data, k):
     
     return point_to_means, means
     
-    
-    def regspace(X , dmin):
-    
-        T = X.shape[1]
-        Y = list()
-        Y.append(X[:,0])
-        
-        for t in range(2,T):
-            if ( np.min ( np.linalg.norm(X[:,t] - Y)) < dmin):
-                Y.append(X[:,t])
-        return np.asarray(Y)
+
