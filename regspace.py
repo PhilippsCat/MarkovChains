09ef8847b106a994a1e00f561b860669f96b46b5
nwulkow <niklas.wulkow@ewetel.net>
2015-07-04 12:05:02 +0200

    regspace with multi-dimensional arrays

--- regspace.py
+++ regspace.py
@@ -4,38 +4,51 @@ def regspace(X , dmin):
 
 
     
-    # Reshape the data 
+    # Reshape the data. It is taken care of the three types of one-dimensional data: [x,y,z], [[x,y,z]] and [ [x], [y], [z]]
     if (len(X.shape) == 2):
-        len_X = max( X.shape[0], X.shape[1])
+        len_X = X.shape[0]
+        if (X.shape[0] == 1 and X.shape[1] > 1):
+            X = X[0]
+            len_X = len(X)
+            X = X.reshape(-1,len_X)
+            X = X.reshape(-1,len(X))
     else:
         len_X = len(X)
-    X = X.reshape(-1,len_X)
+        X = X.reshape(-1,len_X)
+        X = X.reshape(-1,len(X))
+
     T = len_X
     Y = list()
+    X = X.transpose()
     Y.append(X[:,0])
-    
     # Do the regular space algorithm
     for t in range(1,T):
-        if ( np.min ( np.absolute(X[:,t]*np.ones(len(Y)) - Y)) > dmin):
+        if ( np.min ( np.linalg.norm(X[:,t]*np.ones((len(Y),Y[0].shape[0])) - Y, axis = 1)) > dmin):
             Y.append(X[:,t])
      
     # The format of Y is changed from list to np.array
     Y = np.asarray(Y)
 
+    closest_centers = np.zeros((T,Y[0].shape[0]))
     
-    closest_centers = np.zeros(T)
     # values are mapped to their closest centers
     for i in range(0,T):
-        closest_center_index = np.argmin( np.absolute(X[:,i]*np.ones(len(Y)) - Y.transpose()))
-        closest_centers[i] = Y[closest_center_index]
+        closest_center_index = np.argmin( np.linalg.norm(X[:,i]*np.ones((len(Y),Y[0].shape[0])) - Y, axis = 1))
+        closest_centers[i,:] = Y[closest_center_index]
     
-    print closest_centers
-    # 'uniques' is the list of states that occur in 'closest_centers'
-    uniques = list(set(closest_centers))
+    # 'uniques' is the list of states that occur in 'closest_centers'   
+    ncols = closest_centers.shape[1]
+    dtype = closest_centers.dtype.descr * ncols
+    struct = closest_centers.view(dtype)
+    uniques = np.unique(struct)
+    uniques = uniques.view(closest_centers.dtype).reshape(-1, ncols)
+    
     # we map the values in 'closest_centers' to integer values
     int_states = np.zeros(T)
     for j  in range(0,T):
-        int_states[j] = uniques.index(closest_centers[j])
+        na = closest_centers[j,:]
+        int_states[j] = (np.where(np.all(uniques==closest_centers[j,:],axis=1)))[0]
+
 
     
     
