5c19e21a643531749e8d35ed97f95b32b181586e
nwulkow <niklas.wulkow@ewetel.net>
2015-07-04 12:51:30 +0200

    Update mostlikelypath_visu.py

--- mostlikelypath_visu.py
+++ mostlikelypath_visu.py
@@ -1,12 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 def mostlikelypath_visu(start, T, pathlength):
     M = np.zeros((pathlength,T.shape[0]))
-    M[0,:] = start
+    M[0,:] = np.zeros((1,T.shape[0]))
+    M[0,start] = 1
     dispmatrix = M
     for i in range(1,pathlength):
-        M[i,:] = dot(T.transpose(),start)
+        M[i,:] = np.dot(T.transpose(),M[0,:])
         dispmatrix[i,:] = M[i,:]*M[i,:]*M[i,:]*M[i,:]
         dispmatrix[i,:] = dispmatrix[i,:] / sum(dispmatrix[i,:])
-        T = dot(T,T) 
+        T = np.dot(T,T) 
     plt.imshow(dispmatrix.transpose())
     return
+    
