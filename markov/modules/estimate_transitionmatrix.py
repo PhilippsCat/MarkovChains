f1c50ce636f81d30c84c94669bc434db836f4da8
philipp <philipp.donner@fu-berlin.de>
2015-07-09 16:11:44 +0200

    added another estimator to estimate

--- markov/modules/estimate_transitionmatrix.py
+++ markov/modules/estimate_transitionmatrix.py
@@ -42,3 +42,37 @@ def db_estimator(count_matrix, epsilon=1e-8, max_iter=50):
 
     x_new = x_new / np.sum(x_new,1)[:, np.newaxis]  # normalize
     return x_new
+    
+def db_estimator_beta(chain, epsilon=1e-8, max_iter=50):
+    """ 
+    Estimate a reversible transition matrix from a given chain
+    """
+    count_matrix = count_transitions(chain)
+   # x_old = estimate_transitionmatrix(chain) 
+    x_old = np.eye(max(chain) + 1)
+    n = len(x_old)
+    x_new = np.zeros([n,n])
+    c_rows = np.sum(count_matrix,1)
+    iterations = 0
+    x_older = x_new
+    while np.allclose(x_old,x_older,epsilon) == False:
+        x_old_rows = np.sum(x_old,1)        
+        for i in range(n):
+            if x_old_rows[i] == 0:
+                continue            
+            for j in range(n):
+                if (x_old_rows[j] == 0) or (c_rows[i] == 0 and c_rows[j] == 0):
+                    continue                
+                x_new[i,j] = (count_matrix[i,j]+count_matrix[j,i])/(c_rows[i]/x_old_rows[i]+c_rows[j]/x_old_rows[j])
+        iterations = iterations + 1
+        x_older = x_old
+        x_old = x_new
+        if iterations == max_iter:
+            break
+      
+    for i in range(n):
+        row_sum = np.sum(x_new[i,:])
+        if row_sum != 0:
+            x_new[i,:] = x_new[i,:] / row_sum
+    #x_new = x_new / np.sum(x_new,1)[:, np.newaxis]
+    return x_new
